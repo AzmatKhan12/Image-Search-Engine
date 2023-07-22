75a26dafaefefe5bb03cf9ce5cd3ad913e134df5
AzmatKhan12 <azmat.dgms@gmail.com>
2023-07-23 00:54:08 +0530

    Display image in UI

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@
 import './App.css';
 
 
-import React from 'react'
+import React, { createContext, useContext } from 'react'
 import Jumbutron from './components/Jumbutron';
 import SearchField from './components/SearchField';
 import Images from './components/Images';
 import UseAxios from './Hooks/UseAxios';
 
+
+// Create useContext..
+export const ImageContext = createContext();
+
 const App=()=> {
   const {isLoading,response,error,fetchData}=UseAxios(`search/photos?page=1&query=dog&client_id=${process.env.REACT_APP_ACCES_KEY}`)
     
      console.log(response);
+    const value = {
+      isLoading,
+      response,
+      error,
+      fetchData
+    }
+
   return (
-    <>
+    <ImageContext.Provider value={value}>
       <Jumbutron>
          <SearchField/>
       </Jumbutron>
       <Images/>
-    </>
+    </ImageContext.Provider>
   )
 }
 
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ImageContext } from '../App'
+import Image from './Image'
+import Skeleton from './Skeleton'
 
 const Images = () => {
+  const {response,isLoading} =useContext(ImageContext)
   return (
     <div className='text-center mt-6 underline text-2xl'>
           Result for Cats
-          <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid:cols-4 gap-4 my-10'>
-            <h1>Images</h1>
-            <h1>Images</h1>
-            <h1>Images</h1>
-            <h1>Images</h1>
-            <h1>Images</h1>
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid:cols-4 gap-4 my-10
+               max-w-7xl mx-auto px-4'>
+            {!isLoading ? <Skeleton item={12}/> : response.map((data,key)=> <Image key={key} data= {data}/>)}
           </div>
     </div>
   )
